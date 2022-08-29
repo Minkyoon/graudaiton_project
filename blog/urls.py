86ab9ab6992d325e0b88d966c698ceb9ff6f1024
MIN <minkyoon97@hanmail.net>
2022-08-29 16:46:23 +0900

    make blog post page by using CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+
+urlpatterns = [
+
+    path('', views.PostList.as_view()),
+    #path('<int:pk>/', views.single_post_page),
+    # path('', views.index),
 ]
