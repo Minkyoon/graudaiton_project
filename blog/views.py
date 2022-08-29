9fa919e36da566765a7be89723faffc05a1ab31f
MIN <minkyoon97@hanmail.net>
2022-08-29 13:43:36 +0900

    Fmake blog post by FBV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
+
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+
+def index(requset, request=None):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
